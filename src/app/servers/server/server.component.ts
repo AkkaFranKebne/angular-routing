b9ba8ff5c8f62e1fb570aee3902ff1f49638519b
AkkaFranKebne <ewawrobel@gmail.com>
2017-12-17 16:41:13 +0100

    passing parameters in url to another component

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -13,7 +13,8 @@ export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
@@ -22,7 +23,9 @@ export class ServerComponent implements OnInit {
         (params: Params) => {
           this.server = this.serversService.getServer(+params['id']);
         }
-    )
+    );
   }
-
+  onEdit(){
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
+  };
 }
