fa5172b729f7a0a0eab39c3a8c1eed5060ccb5eb
AkkaFranKebne <ewawrobel@gmail.com>
2017-12-17 20:02:18 +0100

    fake login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {AuthService} from "../auth-service";
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,10 @@ import {Router} from "@angular/router";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  status = 'logged out';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -17,5 +20,15 @@ export class HomeComponent implements OnInit {
     // some calculation
     this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
+
+  onLogin() {
+    this.authService.login();
+    this.status = 'logged in';
+  }
+
+    onLogout() {
+        this.authService.logout();
+        this.status = 'logged out';
+    }
 
 }
